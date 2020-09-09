c0d4536c6bf654b305f3c0e6eaf71a004782803d
mig-25 <sohailhasware@MacBook-Pro.local>
2020-09-09 14:00:42 +0200

    Add Content Wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content } from "../components"
 
 const index = () => {
   return (
     <Container>
       <FeatureImage />
-      <h1>hello world</h1>
+      <Content />
     </Container>
   )
 }
