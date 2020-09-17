6ad9aaf7c27e0c2ed6016612f42ca7cb48631a29
mig-25 <sohailhasware@MacBook-Pro-2.local>
2020-09-17 16:08:55 +0200

    update gatsby-plugin-sharp quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 90,
+        quality: 90,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
     {
@@ -39,6 +50,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
+              quality: 90,
             },
           },
         ],
